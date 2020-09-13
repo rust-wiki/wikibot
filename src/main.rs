ccafc1a43febefd36ab98dd3dcaf87df7f42591c
Markus Røyset <maroider@protonmail.com>
2020-09-13 20:05:35 +0200

    Print template transclusions

--- src/main.rs
+++ src/main.rs
@@ -4,26 +4,21 @@
 //! Bot for automating tasks in the Rust wiki
 //!
 //! This is WIP and currently doesn't do anything useful.
-//! It just fetches all pages that include a certain template and prints them.
 
 use std::{error::Error, time::Instant};
 
+use clap::Clap;
 use mediawiki::api::Api;
 use mediawiki::method::Method;
-
-use clap::Clap;
 
 #[macro_use]
 mod macros;
 
-const DEFAULT_API_URL: &str = "https://runrust.miraheze.org/w/api.php";
-const DEFAULT_USER: &str = "Dev-WikiBot";
-const PAGE_ID: &str = "97";
+const PAGE_ID: &str = "56"; // Template:CrateLink
 
 /// 🚧 WIP 🚧
 ///
-/// This currently just requests the page with the ID 97 and prints all pages
-/// where this page is transcluded.
+/// This currently just requests the page with the ID 56 and prints all transclusions of this page.
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let opts = Opts::parse();
@@ -48,6 +43,11 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let result = api.query_api_json(params, Method::Get).await?;
     let page = &result["query"]["pages"][PAGE_ID];
 
+    let template_name = page["title"]
+        .as_str()
+        .unwrap()
+        .trim_start_matches("Template:");
+
     println!("==> Request finished in {:.2?}", time.elapsed());
 
     println!("Page: {}", page["title"]);
@@ -59,6 +59,42 @@ async fn main() -> Result<(), Box<dyn Error>> {
             value["pageid"].as_u64().unwrap(),
             value["title"]
         );
+
+        if let Some(page_id) = value["pageid"].as_u64() {
+            let params = map!(action: "parse", prop: "parsetree", pageid: page_id.to_string());
+            let result = api.query_api_json(params, Method::Get).await?;
+            let parse_tree = result["parse"]["parsetree"]["*"].as_str().unwrap();
+            let parse_tree = roxmltree::Document::parse(parse_tree)?;
+            for node in parse_tree.descendants() {
+                if node.tag_name().name() == "template" {
+                    if node
+                        .children()
+                        .find(|child| child.tag_name().name() == "title")
+                        .and_then(|title_tag| title_tag.text())
+                        .map(|title| title == template_name)
+                        .unwrap_or(false)
+                    {
+                        if let Some(part) = node
+                            .children()
+                            .find(|child| child.tag_name().name() == "part")
+                        {
+                            if let Some(crate_name) = part
+                                .children()
+                                .find(|child| child.tag_name().name() == "name")
+                                .and_then(|name_tag| {
+                                    name_tag
+                                        .next_siblings()
+                                        .find(|sib| sib.tag_name().name() == "value")
+                                })
+                                .and_then(|crate_name| crate_name.text())
+                            {
+                                println!("{:>10}{{{{CrateLink|{}}}}}", "", crate_name);
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 
     Ok(())
@@ -67,10 +103,10 @@ async fn main() -> Result<(), Box<dyn Error>> {
 #[derive(Clap)]
 struct Opts {
     /// Usually "https://<host>/w/api.php"
-    #[clap(long, env, default_value=DEFAULT_API_URL)]
+    #[clap(long, env, default_value = "https://runrust.miraheze.org/w/api.php")]
     api_url: String,
     /// The username of the bot account
-    #[clap(long, env, default_value=DEFAULT_USER)]
+    #[clap(long, env, default_value = "Dev-WikiBot")]
     username: String,
     /// The password of the bot account
     #[clap(long, env)]
